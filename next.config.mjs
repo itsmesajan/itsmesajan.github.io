cdb6c93fc279adfa70145630d86045363a703ac0
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2025-05-04 20:54:22 +0545

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  assetPrefix: "itsmesajan.github.io", // if deploying to user.github.io/REPO_NAME
+};
 
 export default nextConfig;
